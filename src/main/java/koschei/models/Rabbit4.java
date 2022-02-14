1c5df7f751c8e41561cfb68ef3e25b223587317e
RavQs <ravenq1112@gmail.com>
2022-02-14 14:16:51 +0900

    stucc

--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -1,9 +1,16 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Rabbit4 {
+    @Autowired
+    private Duck5 duck;
+
+    public Rabbit4(Duck5 duck) {
+        this.duck = duck;
+    }
 
     @Override
     public String toString() {
